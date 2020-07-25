5be7ed1e9ad90888250a395869e869fb916c055b
Philip <philsavenok@gmail.com>
2020-07-25 17:24:39 -0400

    renaming Home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Navbar from './shared/Navbar'
 import Footer from './shared/Footer'
-import Home from './Home/Index'
+import Home from './home/Home'
 import './App.css'
 
 function App() {
---
+++ src/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div id='fullpage'>
+      <section className='section s1'>
+        <h1>Section 1</h1>
+      </section>
+      <section className='section s2'>
+        <h1>Section 2</h1>
+      </section>
+      <section className='section s3'>
+        <h1>Section 3</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Home
