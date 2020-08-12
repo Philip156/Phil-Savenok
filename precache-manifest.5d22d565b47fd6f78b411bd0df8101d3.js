809b6163122e4345c969fe7a174be0d1e40ae1d1
Philip <philsavenok@gmail.com>
2020-08-12 19:03:34 -0400

    Updates

---
+++ precache-manifest.5d22d565b47fd6f78b411bd0df8101d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1259aa92a53f37620ec74e709d26ba7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9f6ff988bd79cc508247",
+    "url": "/Portfolio/static/css/main.daafb733.chunk.css"
+  },
+  {
+    "revision": "8b0ff2687537676e23ca",
+    "url": "/Portfolio/static/js/2.6dcbfeea.chunk.js"
+  },
+  {
+    "revision": "90557f46eab5f97fe6922971bf2d4111",
+    "url": "/Portfolio/static/js/2.6dcbfeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6ff988bd79cc508247",
+    "url": "/Portfolio/static/js/main.d612a157.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "fde3c73b5574ed657a9b5c2b51a2500e",
+    "url": "/Portfolio/static/media/AltusCapital.fde3c73b.png"
+  },
+  {
+    "revision": "f9f8e181a8ef174588044b8a1b870ef4",
+    "url": "/Portfolio/static/media/AmerraCapital.f9f8e181.png"
+  },
+  {
+    "revision": "565e9a51dc40fec3ec0761fa6db1034f",
+    "url": "/Portfolio/static/media/SunwaterCapital.565e9a51.png"
+  },
+  {
+    "revision": "fa7f03ed89fb077048747992da8d2f24",
+    "url": "/Portfolio/static/media/footer.fa7f03ed.jpg"
+  },
+  {
+    "revision": "d11058d763139385b0efba2e021f69fc",
+    "url": "/Portfolio/static/media/landing.d11058d7.jpg"
+  }
+]);
